70272b9ea21927f1fb9f3074dad9ecad26c03bdd
yanceyy <yanceyy@users.noreply.github.com>
2023-02-24 13:45:06 +1000

    use swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
 import { defineConfig } from 'vite';
-import msw from "@iodigital/vite-plugin-msw";
-import react from '@vitejs/plugin-react';
-import handlers from "./src/mocks/handlers";
+import msw from '@iodigital/vite-plugin-msw';
+import react from '@vitejs/plugin-react-swc';
 import autoprefixer from 'autoprefixer';
 import eslint from 'vite-plugin-eslint';
+import handlers from './src/mocks/handlers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [msw({ handlers }), react({
-    jsxImportSource: "@emotion/react",
-    babel: {
-      plugins: ["@emotion/babel-plugin"],
-    },
-  }), eslint()],
+  plugins: [
+    msw({ handlers }),
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    eslint(),
+  ],
   css: {
     postcss: {
       plugins: [
-        autoprefixer({}) // add options if needed
+        autoprefixer({}), // add options if needed
       ],
-    }
-  }
+    },
+  },
 });
